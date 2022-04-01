87d99d0c27d862342ad10a8b33371bd38c17723e
Sebastian <sebastian.legnazzi@est.fi.uncoma.edu.ar>
2022-04-01 11:20:12 -0300

    Creo los set, get y construct

---
+++ Viaje.php
@@ -0,0 +1,112 @@
+<?php
+class Viaje{
+    private $codigoViaje;
+    private $destino;
+    private $cantidadMax;
+    private $pasajeros;
+    
+    
+
+    /**************************************/
+    /**************** SET *****************/
+    /**************************************/
+
+    /**
+     * Cambia el valor de cantidad maxima
+     *
+     * @param int $cantidadMax
+     */ 
+    public function setCantidadMax($cantidadMax){
+        $this->cantidadMax = $cantidadMax;
+    }
+
+    /**
+     * Cambia el valor de destino
+     *
+     * @param string $destino
+     */ 
+    public function setDestino($destino){
+        $this->destino = $destino;
+    }
+
+    /**
+     * Cambia el valor del codigo del viaje
+     *
+     * @param int $codigoViaje
+     */ 
+    public function setCodigoViaje($codigoViaje){
+        $this->codigoViaje = $codigoViaje;
+    }
+
+    /**
+     * Cambia el valor de los pasajeros
+     *
+     * @param array $pasajeros
+     */ 
+    public function setPasajeros($pasajeros){
+        $this->pasajeros = $pasajeros;
+    }
+
+
+    /**************************************/
+    /**************** GET *****************/
+    /**************************************/
+
+    /**
+     * Devuelve el array con la cantidad de pasajeros
+     * 
+     * @return array
+     */ 
+    public function getPasajeros(){
+        return $this->pasajeros;
+    }
+
+    /**
+     * Devuelve la cantidad maxima de pasajeros
+     * 
+     * @return int
+     */ 
+    public function getCantidadMax(){
+        return $this->cantidadMax;
+    }
+
+    /**
+     * Devuelve el nombre del viaje
+     * 
+     * @return  string
+     */ 
+    public function getDestino(){
+        return $this->destino;
+    }
+
+    /**
+     * Devuelve el codigo del viaje
+     * 
+     * @return int
+     */ 
+    public function getCodigoViaje(){
+        return $this->codigoViaje;
+    }
+
+    /**************************************/
+    /************** FUNCIONES *************/
+    /**************************************/
+
+    /**
+     * Este modulo asigna los valores a los atributos cuando se crea una instancia de la clase 
+     * @param array $pasajeros
+     * @param int $cantidadMax
+     * @param string $destino
+     * @param int $codigoViaje
+    */
+    public function __construct($pasajeros,$cantidadMax,$destino,$codigoViaje){
+        $this->pasajeros = $pasajeros;
+        $this->cantidadMax = $cantidadMax;
+        $this->destino = $destino;
+        $this->codigoViaje = $codigoViaje;
+    }
+    
+
+}
+
+?>
